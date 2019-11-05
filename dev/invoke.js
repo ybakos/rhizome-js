21450d728cb0db3870ba6ed78c3c30023fa0f49c
Dylan Drudge <drudged@oregonstate.edu>
2019-11-04 20:14:12 -0800

    get as far as possible with tui from blessed, moving to oclif cli from heroku

--- dev/invoke.js
+++ dev/invoke.js
@@ -1,20 +1,13 @@
 const Redis = require('redis');
 const IPFSAPI = require('ipfs-http-client');
 const _ = require('lodash');
-const { PublicFileManager } = require('../index');
+const { PublicFileManager, tui } = require('../index');
 
-let ipfs;
+( async()=>{
+  const ipfs = await new IPFSAPI('127.0.0.1', 5001);   
+  const redis = Redis.createClient({host: 'localhost'});
+  const PFM = await new PublicFileManager(ipfs,redis);
 
-if (process.env.DOCKER) {
-  // const IPFSFactory = require('ipfsd-ctl');
-  // const f = IPFSFactory.create();
-  // const ipfsd = await f.spawn();
-  // ipfs = ipfsd.api;
-  ipfs = await new IPFSAPI('ipfs.infura.io', 5001, {protocol: 'https'});    
-}else{
-  ipfs = await new IPFSAPI('127.0.0.1', 5001);
-};    
-
-const redis = Redis.createClient({host: redisHost});
-const PFM = await new PublicFileManager(ipfs,redis);
-
+  // tui();
+})();
+  
