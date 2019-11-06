e57cfa3a170b32bf5258261bf2a5644bc6c3b645
Dylan Drudge <drudged@oregonstate.edu>
2019-11-05 21:39:37 -0800

    refactor dev invoke to return an instance of pfm

--- dev/invoke.js
+++ dev/invoke.js
@@ -1,13 +1,15 @@
 const Redis = require('redis');
 const IPFSAPI = require('ipfs-http-client');
 const _ = require('lodash');
-const { PublicFileManager, tui } = require('../index');
+const { PublicFileManager } = require('../index');
 
-( async()=>{
+const localHostRunner = async()=>{
   const ipfs = await new IPFSAPI('127.0.0.1', 5001);   
   const redis = Redis.createClient({host: 'localhost'});
   const PFM = await new PublicFileManager(ipfs,redis);
+  
+  return PFM;
+}
 
-  // tui();
-})();
+module.exports = localHostRunner;
   
