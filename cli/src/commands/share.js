bc2c06b34fc0d80fca4b2d90c416463eb4bff96d
Dylan Drudge <drudged@oregonstate.edu>
2019-11-05 18:56:12 -0800

    cli from heroku

---
+++ cli/src/commands/share.js
@@ -0,0 +1,38 @@
+const { Command, flags } = require('@oclif/command');
+const PFMCreator = require('../../../dev/invoke');
+
+const getInput = async(question) => {
+  const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  return new Promise((reject, resolve)=> {
+    readline.question(question ,async(input) => {
+      readline.close();
+      resolve(input);
+    });
+  });
+};
+
+class ShareCommand extends Command {
+  async run() {
+    const {flags} = this.parse(ShareCommand);
+    // const PFM = await PFMCreator();
+    const message = flags.name || await getInput('Data: ');
+    console.log(message)
+    // const uploadedMessage = PFM.upload(message);
+  }
+}
+
+ShareCommand.description = `Share text-based information with your cohorts.`
+
+ShareCommand.flags = {
+  name: flags.string({
+    char: 'm', description: 'Message to send.'
+  }),
+  tags: flags.string({
+    char: 't', description: 'Tags to add.'
+  })
+}
+
+module.exports = ShareCommand
