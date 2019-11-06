6735029fc7c06ed90c2e9d3b29ace1f32e0b8e78
Dylan Drudge <drudged@oregonstate.edu>
2019-11-05 21:37:20 -0800

    add runner for tui

---
+++ dev/tui.js
@@ -0,0 +1,13 @@
+const Redis = require('redis');
+const IPFSAPI = require('ipfs-http-client');
+const _ = require('lodash');
+const { PublicFileManager, tui } = require('../index');
+
+(async()=>{
+  const ipfs = await new IPFSAPI('127.0.0.1', 5001);   
+  const redis = Redis.createClient({host: 'localhost'});
+  const PFM = await new PublicFileManager(ipfs,redis);
+  
+  tui(PFM);
+}
+)();
