e1aac587e245029b22d05d0add8111e963faa23a
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-11-26 23:19:38 -0800

    Thing & Continuum: Sketch out initial API.
    The Continuum is the data store. It's essentially a graph, but instead:
    1) Stores Things in an array, allowing array indices to serve as URIs.
       Since URI schemes are really numbers anyway, just use the monotonically
       increasing indices of the array to represent URIs.
    
    2) Information can exist without being a 'Thing' yet. For example, opening a
       text buffer allows us to create some information (data). It's not until we
       'save' it that it becomes information-as-thing.
    
    3) We store information in the Continuum, and the Continuum transforms it
       into a Thing (information object).
    
    A Thing is an information object, containing the information itself, a URI
    and 'tags', which is a list of URIs of other Things associated with it.

--- __tests__/continuum.test.js
+++ __tests__/continuum.test.js
@@ -1,5 +1,23 @@
 const Continuum = require('../lib/continuum')
 
 test('The continuum exists', () => {
-  const continuum = new Continuum()
+  const c = new Continuum()
+})
+
+test('The continuum represents an infinite set of Things', () => {
+  const c = new Continuum([])
+})
+
+test('Storing information in the continuum returns a Thing with a URI', () => {
+  const c = new Continuum()
+  thing = c.store('fake')
+  expect(thing.information).toBe('fake')
+  expect(thing.uri).toBe(0)
+  expect(thing.tags).toEqual([])
+})
+
+test('You retrieve a thing via its uri', () => {
+  const c = new Continuum()
+  thing = c.store('fake')
+  expect(c.get(thing.uri)).toBe(thing)
 })
