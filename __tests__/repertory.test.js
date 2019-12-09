b96e322f8c548bdaf8e5b31c0ef28359c2f33230
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-12-08 21:33:48 -0800

    Repertory: Provide search index.
    The system needs a way to 'find information', thereby returning the uri
    of a Thing that represents that information. It is essentially a search
    index or catalog.
    
    In history, the Mundaneum's massive 'card catalog' was named by Otlet as
    the Repertoire Bibliographique Universel (RBU) or 'Universal Bibliographic
    Repertory.'

---
+++ __tests__/repertory.test.js
@@ -0,0 +1,25 @@
+const Repertory = require('../lib/repertory')
+
+test('The repertory exists', () => {
+  const m = new Repertory()
+})
+
+test('The repertory represents a searchable catalog of information', () => {
+  const m = new Repertory({})
+  expect(m).toHaveProperty('index')
+})
+
+test('Registering a thing stores its information in the index', () => {
+  const m = new Repertory({})
+  m.register(fake_thing())
+  expect(m.index[fake_thing().information()]).toEqual(fake_thing().uri())
+})
+
+test('Querying the repertory with existing information returns a uri', () => {
+  const m = new Repertory({'fake': 0})
+  expect(m.query('fake')).toEqual(0)
+})
+
+function fake_thing() {
+  return { information: function() { return 'fake'} , uri: function() { return 0 } }
+}
